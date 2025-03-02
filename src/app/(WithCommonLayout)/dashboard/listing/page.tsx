b08ac5cf790dcb13a59038f8fab9616f0ebd2412
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-02 21:46:48 +0600

    start talbe for user data show

--- src/app/(WithCommonLayout)/dashboard/listing/page.tsx
+++ src/app/(WithCommonLayout)/dashboard/listing/page.tsx
@@ -1,15 +1,18 @@
 import ManageProducts from "@/components/modules/dashboard/product";
 import Sidebar from "@/components/modules/dashboard/sidebar";
 import SHContainer from "@/components/ui/core/SHContainer";
+import { getAllUserProducts } from "@/services/Product";
 
 
-const ListingPage = () => {
+const ListingPage = async ({ searchParams }: { searchParams: Promise<{ page: string }> }) => {
+    const { page } = await searchParams
+    const { data, meta } = await getAllUserProducts(page, "1");
     return (
          <div>
             <Sidebar />
             <div className='bg-[#f8fafd]'>
                 <SHContainer >
-                    <ManageProducts />
+                    <ManageProducts meta={meta} products={data} />
                 </SHContainer>
             </div>
         </div>
--- src/services/Product/index.ts
+++ src/services/Product/index.ts
@@ -65,5 +65,21 @@ export const getAllProducts = async (
 };
 
 
-
+// get all products
+export const getAllUserProducts = async (page?: string, limit?: string) => {
+    try {
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_BASE_API}/product?limit=${limit}&page=${page}`,
+            {
+                next: {
+                    tags: ["PRODUCT"],
+                },
+            }
+        );
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        return Error(error.message);
+    }
+};
 
