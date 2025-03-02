5e2d67c76984149c63e9bb2867538239087fd738
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-02 14:44:50 +0600

    sidebar active done

--- src/components/modules/dashboard/sidebar/index.tsx
+++ src/components/modules/dashboard/sidebar/index.tsx
@@ -1,21 +1,37 @@
-
-
+"use client"
+import { usePathname } from 'next/navigation';
 import SHContainer from '@/components/ui/core/SHContainer';
 import Link from 'next/link';
 
 const Sidebar = () => {
+    const pathname = usePathname();
+
+    const isActive = (path: string) => {
+        return pathname === path ? 'text-blue-500 font-semibold' : 'text-gray-700';
+    };
+
     return (
-        <div className='w-full h-full bg-[#fdfdfe] shadow-sm '>
-            <SHContainer className=" py-10 flex items-end gap-10 justify-start">
-                <Link href="/dashboard/listing/add-ads" className="text-gray-700 hover:text-blue-500 text-lg">Add New</Link>
-                <Link href="/dashboard/listing" className="text-gray-700 hover:text-blue-500 text-lg">My Ads</Link>
-                <Link href="/favorites" className="text-gray-700 hover:text-blue-500 text-lg">Favorites</Link>
-                <Link href="/messages" className="text-gray-700 hover:text-blue-500 text-lg">Messages</Link>
-                <Link href="/dashboard/profile" className="text-gray-700 hover:text-blue-500 text-lg">Settings</Link>
+        <div className="w-full h-full bg-[#fdfdfe] shadow-sm">
+            <SHContainer className="py-10 flex items-end gap-10 justify-start">
+                {/* Navigation Links */}
+                <Link className={`${isActive('/dashboard/listing/add-ads')} text-lg`} href="/dashboard/listing/add-ads">
+                    Add New
+                </Link>
+                <Link className={`${isActive('/dashboard/listing')} text-lg`} href="/dashboard/listing">
+                    My Ads
+                </Link>
+                <Link className={`${isActive('/favorites')} text-lg`} href="/favorites">
+                    Favorites
+                </Link>
+                <Link className={`${isActive('/messages')} text-lg`} href="/messages">
+                    Messages
+                </Link>
+                <Link className={`${isActive('/dashboard/profile')} text-lg`} href="/dashboard/profile">
+                    Settings
+                </Link>
             </SHContainer>
-       </div>
+        </div>
     );
 }
 
 export default Sidebar;
-
