b8caee953f86c33a9427c7c1c938ac0d2dabf1a9
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-05 15:27:34 +0600

    contact us done

--- src/app/(WithCommonLayout)/contact-us/page.tsx
+++ src/app/(WithCommonLayout)/contact-us/page.tsx
@@ -1,9 +1,11 @@
 import ContactForm from "@/components/modules/contact";
+import ContactLocation from "@/components/modules/contact/ContactLocation";
 
 const ContactUsPage = () => {
     return (
         <div>
             <ContactForm />
+            <ContactLocation />
         </div>
     );
 };
