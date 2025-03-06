3ef4cf6328d8056dc5700fb026d986b2ce3d92ec
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-06 21:53:29 +0600

    done

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,7 +27,7 @@ export const middleware = async (request: NextRequest) => {
         } else {
             return NextResponse.redirect(
                 new URL(
-                    `http://localhost:3000/login?redirectPath=${pathname}`,
+                    `https://second-hand-client-dc3y.vercel.app/login?redirectPath=${pathname}`,
                     request.url
                 )
             );
