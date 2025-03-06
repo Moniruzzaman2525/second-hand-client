3e46f2f9d2fd31baf530515be7efb83c3490e01c
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-06 17:45:23 +0600

    verfiy done

--- src/services/AuthService/index.ts
+++ src/services/AuthService/index.ts
@@ -74,6 +74,22 @@ export const resetPassword = async (id: string | undefined, token: string | unde
     return Error(error);
   }
 };
+export const verifyEmail = async (id: string | undefined, token: string | undefined) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/auth/reset-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `${token}`,
+      },
+      body: JSON.stringify({ id }),
+    });
+    const result = await res.json();
+    return result;
+  } catch (error: any) {
+    return Error(error);
+  }
+};
 
 export const getCurrentUser = async () => {
   const accessToken = (await cookies()).get("accessToken")?.value;
