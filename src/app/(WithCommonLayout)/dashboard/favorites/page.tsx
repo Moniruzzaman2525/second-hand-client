ce3df1267e532085903c65faaba28e3aa67078f2
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-03-06 08:54:49 +0600

    update favorite

--- src/app/(WithCommonLayout)/dashboard/favorites/page.tsx
+++ src/app/(WithCommonLayout)/dashboard/favorites/page.tsx
@@ -7,10 +7,11 @@ type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 const ProductsPage = async ({ searchParams }: { searchParams: SearchParams }) => {
     const query = await searchParams;
     const page = query.page as string | undefined;
-    const { data: products, meta } = await getUserWishlist(page, "6",);
+    const { data: products } = await getUserWishlist(page, "6",);
+
     return (
         <SHContainer>
-            <ViewWishlist  meta={meta} products={products?.result} />
+            <ViewWishlist  meta={products.meta} products={products?.result} />
         </SHContainer>
     );
 };
