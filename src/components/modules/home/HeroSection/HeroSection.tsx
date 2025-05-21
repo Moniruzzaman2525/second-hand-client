521b1855c41d015c4d2a768714cbce8e58514781
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-05-21 16:45:47 +0600

    fixed hero section in 70% visible

--- src/components/modules/home/HeroSection/HeroSection.tsx
+++ src/components/modules/home/HeroSection/HeroSection.tsx
@@ -5,9 +5,8 @@ const HeroSection = () => {
 
 
     return (
-        <div
-            className={`${styles.banner} relative w-full h-screen flex flex-col items-center justify-center text-center bg-cover bg-center`}
-        >
+        <div className={`${styles.banner} relative w-full h-full flex flex-col items-center justify-center text-center bg-cover bg-center`}>
+
             <div className="absolute inset-0 bg-[#374B5C] bg-opacity-50 backdrop-blur-[5px]"></div>
 
             <div className="relative z-10">
